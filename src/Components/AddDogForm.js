1a35cafd9442829712fac87af21ade7caf8852f9
Teffisk <brad.mcknight91@gmail.com>
2019-02-27 10:41:03 -0800

    add new dog form working

--- src/Components/AddDogForm.js
+++ src/Components/AddDogForm.js
@@ -1,29 +1,49 @@
 import React, { Component } from "react";
-
-handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({dog: })
-    this.createNewDog();
-};
-
-storeInput = (e) => {
-    this.setState({
-        [e.target.name]: e.target.value
-    })
-}
-
-createNewDog = () => {
-    console.log("Hitting createNewDog fetch call");
-    fetch(SERVER_URL+'/habits/'+this.props.user.id, {
-        method: 'POST',
-        body: JSON.stringify(this.state.dog), // data to send to server
-        headers: {
-            'Content-Type': 'application/json' // let the server know what's coming
-        }
-    })
-};
+import SERVER_URL from "../constants/server";
 
 class AddDogForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      breed: "",
+      gender: "",
+      birthday: "",
+      userId: props.user.id,
+      bio: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.createNewDog();
+  };
+
+  storeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  createNewDog = () => {
+    console.log("Hitting createNewDog fetch call");
+    fetch(SERVER_URL + "/dogs/new", {
+      method: "POST",
+      body: JSON.stringify(this.state), // data to send to server
+      headers: {
+        "Content-Type": "application/json" // let the server know what's coming
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({ dog: json });
+      })
+      .catch(err => {
+        console.log("Error posting data!", err);
+      });
+  };
+
   render() {
     return (
       <div>
@@ -35,23 +55,31 @@ class AddDogForm extends Component {
           </div>
           <div>
             <label htmlFor="birthday">Birthday</label>
-            <input type="date" name="birthday" />
+            <input type="date" name="birthday" onChange={this.storeInput} />
           </div>
-          <input type="hidden" value={this.props.user.id} name="userId" onChange={this.storeInput} />
+          <input
+            type="hidden"
+            value={this.props.user.id}
+            name="userId"
+            onChange={this.storeInput}
+          />
           <div>
             <label htmlFor="breed">breed</label>
             <input type="text" name="breed" onChange={this.storeInput} />
           </div>
           <div>
             <label htmlFor="gender">Gender</label>
-            <select name="gender" onChange={this.storeInput} >
+            <select name="gender" onChange={this.storeInput}>
+              <option disabled="disabled" selected="true">
+                Choose
+              </option>
               <option value="male">M</option>
               <option value="female">F</option>
             </select>
           </div>
           <div>
             <label htmlFor="bio">Tell people about your pup!</label>
-            <textbox name="bio" onChange={this.storeInput} />
+            <textarea name="bio" onChange={this.storeInput} />
           </div>
           <div>
             <input type="submit" />
